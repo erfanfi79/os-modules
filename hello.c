01615ba14ecf2ce42ecfcb13b053f5564b9d5f00
erfanfi79 <erfanfaravani@gmail.com>
2022-10-28 23:17:46 +0330

    change

--- hello.c
+++ hello.c
@@ -2,13 +2,13 @@
     #include <linux/module.h>
 
     static int hello_init(void){
-        printk(KERN_ALERT "TEST: Hello world\n")
+        printk(KERN_ALERT "TEST: Hello world\n");
         return 0;
     }
 
     static void hello_exit(void){
-        printk(KERN_ALERT "TEST: GOOD Buy\n")
+        printk(KERN_ALERT "TEST: GOOD Buy\n");
     }
 
-    module_init(hello_init)
-    module_exit(hello_exit)
+    module_init(hello_init);
+    module_exit(hello_exit);
