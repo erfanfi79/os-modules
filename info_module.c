63a51588d4813c5c02032adb456d5da0d8edb1c3
erfanfi79 <erfanfaravani@gmail.com>
2022-10-28 23:49:02 +0330

    ignore time

--- info_module.c
+++ info_module.c
@@ -1,6 +1,6 @@
     #include <linux/init.h>
     #include <linux/module.h>
-    #include <linux/time.h>
+    // #include <linux/time.h>
 
     MODULE_LICENSE("GPL");
     MODULE_AUTHOR("Erfan Faravani GNU/Linux");
@@ -8,24 +8,24 @@
 
     static int info_init(void){
         printk(KERN_INFO "Name: Erfan\n LastName: Faravani\n StudentId: 97102174\n");
-        unsigned long get_time;
-        int sec, hr, min, tmp1,tmp2, tmp3;
-        struct timeval tv;
-        struct tm tv2;
+        // unsigned long get_time;
+        // int sec, hr, min, tmp1,tmp2, tmp3;
+        // struct timeval tv;
+        // struct tm tv2;
 
-        do_gettimeofday(&tv);
-        get_time = tv.tv_sec;
-        sec = get_time % 60;
-        tmp1 = get_time / 60;
-        min = tmp1 % 60;
-        tmp2 = tmp1 / 60;
-        hr = (tmp2 % 24) - 4;
-        time_to_tm(get_time, 0, &tv2);
-        tmp3 = tv2.tm_year;
+        // do_gettimeofday(&tv);
+        // get_time = tv.tv_sec;
+        // sec = get_time % 60;
+        // tmp1 = get_time / 60;
+        // min = tmp1 % 60;
+        // tmp2 = tmp1 / 60;
+        // hr = (tmp2 % 24) - 4;
+        // time_to_tm(get_time, 0, &tv2);
+        // tmp3 = tv2.tm_year;
 
-        printk(KERN_INFO "time ::  %d:%d:%d\n",hr,min,sec);
-        /* Add years since 1900. */
-        printk(KERN_INFO "Year: %d\n", tmp3 + 1900);
+        // printk(KERN_INFO "time ::  %d:%d:%d\n",hr,min,sec);
+        // /* Add years since 1900. */
+        // printk(KERN_INFO "Year: %d\n", tmp3 + 1900);
         return 0;
     }
 
